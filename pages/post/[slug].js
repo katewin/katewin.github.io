ac14fedec6f5aa1b0a17eb05f698a426a8e0a844
marwinburesch <marwin@posteo.de>
2020-10-10 12:18:24 +0200

    Add md parsing

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,9 +1,19 @@
-export default function PostTemplate(props) {
-  return <div>Where is "{props.slug}"?</div>;
+import matter from "gray-matter";
+
+export default function PostTemplate({ content, data }) {
+  const frontmatter = data;
+  return (
+    <>
+      <h1>{frontmatter.title}</h1>
+      <p>{content}</p>
+    </>
+  );
 }
 
 PostTemplate.getInitialProps = async (context) => {
   const { slug } = context.query;
+  const content = await import(`../../content/${slug}.md`);
+  const data = matter(content.default);
 
-  return { slug };
+  return { ...data };
 };
